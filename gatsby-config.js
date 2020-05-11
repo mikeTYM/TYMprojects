affc58dd7a3f48551ae8bd53d3c280a3d28da615
Mike Renne <64613364+mikeTYM@users.noreply.github.com>
2020-05-11 15:03:47 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Projects",
+    description: 'A collection of projects for members/guests of Triad Youth Makers.',
   },
   plugins: ["gatsby-theme-edith"],
   pathPrefix: "/TYMprojects",
