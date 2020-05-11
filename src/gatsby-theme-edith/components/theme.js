1900069624c38f69363d0c2a1dd960853840cb13
Mike Renne <64613364+mikeTYM@users.noreply.github.com>
2020-05-11 14:45:15 -0400

    Create theme.js

---
+++ src/gatsby-theme-edith/components/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles"
+
+export default theme =>
+ createMuiTheme({
+   ...theme,
+   palette: {
+     primary: {
+       main: "#ff0000",
+   },
+ },
+})
